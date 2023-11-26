6226910c04fdb49eb798b7320c644039c5427e5b
Traalee-IT <letraxinh2510@gmail.com>
2023-11-26 21:30:49 +0700

    helo

---
+++ WedNangCao/ShopWatch/ShopWatch/ShopWatch/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopWatch.Models;
+namespace ShopWatch.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        private DHEntities db = new DHEntities();
+        public ActionResult homeIndex(string searchValue)
+        {
+            var items = db.MATHANGs.Where(m => m.TRANGTHAI != true).AsQueryable();
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                items = items.Where(x =>
+                       SqlFunctions.StringConvert((double)x.GIAHANG).Contains(searchValue) ||
+                       x.TENHANG.Contains(searchValue) ||
+                       SqlFunctions.PatIndex("%" + searchValue + "%", SqlFunctions.StringConvert((double)x.GIAHANG)) > 0 ||
+                       SqlFunctions.PatIndex("%" + searchValue + "%", x.TENHANG) > 0
+                   );
+            }
+            else
+            {
+                var pagedData = items.ToList();
+                return View(pagedData);
+            }
+
+            return View(items.ToList());
+        }
+      
+       
+       public ActionResult danhsach()
+        {
+            return View();
+        }
+    }
+}
---
+++ WedNangCao/ShopWatch/ShopWatch/ShopWatch/Models/MetaData/MATHANG_METADATA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace ShopWatch.Models.MetaData
+{
+    public class MATHANG_METADATA {
+
+        public int MAMATHANG { get; set; }
+        [Required(ErrorMessage = "Please enter the Name")]
+        public string TENHANG { get; set; }
+        /* [Required(ErrorMessage = "Please choose Image")]*/
+        public string ANHSANPHAM { get; set; }
+        [Required(ErrorMessage = "Please Enter date")]
+        
+        public Nullable<double> GIAHANG { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập LOAI.")]
+        public string LOAI { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập BẢO HÀNH")]
+        public string BAOHANH { get; set; }
+        [Required(ErrorMessage = "Vui lòng chọn kich thược")]
+        public string KICHTHUOC { get; set; }
+
+    }
+}
